801089c5e28419acfff6e78909c762b8ebd49afe
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-25 22:30:45 +0000

    added draw function

--- player.py
+++ player.py
@@ -1,5 +1,12 @@
 import pygame
-class Player()
-  def __init__(self):
-    pass
+class Player():
+  def __init__(self, symbol, win):
+    self.symbol = symbol
+    self.win = win
+    
+  def draw(self, coord):
+    pygame.draw.circle(self.win, (0,0,0), coord, 1, 1)
+    
+  
+    
  

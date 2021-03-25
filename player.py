9a027f1a1513a07bf2fde1fa104b5f6a9b7d3668
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-25 22:50:39 +0000

    Update player.py

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player():
   def __init__(self, symbol, win):
     self.symbol = symbol
     self.win = win
+    self.moves = []
     
   def draw(self, coord):
     pygame.draw.circle(self.win, (0,0,0), coord, 1, 1)

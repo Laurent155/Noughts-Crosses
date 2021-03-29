c6d53e3fb5cf9032ba98bdf49fcbf7a719bbf46e
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-29 23:11:21 +0100

    Update client.py

--- client.py
+++ client.py
@@ -16,35 +16,38 @@ def redrawWindow(win, p1, p2):
     pygame.draw.line(win, (0, 0, 0), (201, 0), (201, 602), 1)
     pygame.draw.line(win, (0, 0, 0), (402, 0), (402, 602), 1)
     for i in p1.moves:
-        p1.draw(i, win)
+        if i != (100, 100):
+            p1.draw(i, win)
     for i in p2.moves:
-        p2.draw(i, win)
+        if i != (100, 100):
+            p2.draw(i, win)
     pygame.display.update()
 
 
 def get_coord(tup):
-    return tup*201 + 100
+    return tup[0] // 201, tup[1] // 201
+
+
 
 
 def main():
     run = True
-    circle_turn = True
-    cross_turn = False
     n = Network()
     p1 = n.getP()
-    pos = (1, 1)
+    pos = (100, 100)
+
     while run:
         p2 = n.send(p1)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if p1.turn and event.type == pygame.MOUSEBUTTONDOWN:
                 pos = get_coord(pygame.mouse.get_pos())
+                p1.turn = not p1.turn
         if pos not in p1.moves and pos not in p2.moves:
             p1.moves.append(pos)
-            circle_turn = not circle_turn
-            cross_turn = not cross_turn
+
 
         redrawWindow(win, p1, p2)
 

24778b2ee7c2b1c4720602d8d32232fbe159a4f4
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-29 18:38:18 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,16 +1,15 @@
 import pygame
 
 
-class Player():
-    def __init__(self, symbol, win):
+class Player:
+    def __init__(self, symbol):
         self.symbol = symbol
-        self.win = win
         self.moves = []
 
-    def draw(self, coord):
+    def draw(self, coord, win):
         coord = coord * 201 + 100
         if self.symbol == "circle":
-            pygame.draw.circle(self.win, (0, 0, 0), coord, 1, 1)
+            pygame.draw.circle(win, (0, 0, 0), coord, 1, 1)
         else:
-            pygame.draw.line(self.win, (0, 0, 0), (coord[0] - 20, coord[1] - 20), (coord[0] + 20, coord[1] + 20), 1)
-            pygame.draw.line(self.win, (0, 0, 0), (coord[0] + 20, coord[1] - 20), (coord[0] - 20, coord[1] + 20), 1)
+            pygame.draw.line(win, (0, 0, 0), (coord[0] - 20, coord[1] - 20), (coord[0] + 20, coord[1] + 20), 1)
+            pygame.draw.line(win, (0, 0, 0), (coord[0] + 20, coord[1] - 20), (coord[0] - 20, coord[1] + 20), 1)

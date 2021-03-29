e1cf711af308220d709870ce48669a84b013440d
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-29 18:38:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
-import sys
+import pickle
+from player import Player
 
 # this is the(a) server script, it always has to be running, need to first run this server
 # script, and we can try to connect clients to it
@@ -22,29 +23,20 @@ s.listen(2)
 print("Waiting for a connection, Server Started")
 
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-
-pos = [(0, 50), (100, 200)]
+players = [Player("circle"), Player("cross")]
 
 
 # defining a threaded function
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     # want to send a message to confirm the connection
 
     reply = ""
     while True:
         try:
             # we're putting in 2048 bits, the amount of info we're receiving
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
             # we need to decode the information received, as it is encoded over a client server system
             # utf-8 is the format'''
             # reply = data.decode("utf-8")
@@ -54,14 +46,14 @@ def threaded_client(conn, player):
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
 
                 print("Received: ", data)
                 print("Sending:", reply)
             # this is just encoding it into a bites object
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 

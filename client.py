841b1d06e46c728cb2133e92e76afcdb9cd282a2
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-31 16:57:46 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import pygame
 from player import Player
 from network import Network
 import pickle
+from player import PlayerTurn
 
 width = 602
 height = 602
@@ -28,26 +29,25 @@ def get_coord(tup):
     return tup[0] // 201, tup[1] // 201
 
 
-
-
 def main():
     run = True
     n = Network()
     p1 = n.getP()
     pos = (100, 100)
-
     while run:
         p2 = n.send(p1)
+        if not p2.turn:
+            p1.turn = True
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            if p1.turn and event.type == pygame.MOUSEBUTTONDOWN:
-                pos = get_coord(pygame.mouse.get_pos())
-                p1.turn = not p1.turn
+            if p1.turn:
+                if not p2.turn and event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = get_coord(pygame.mouse.get_pos())
+                    p1.turn = False
         if pos not in p1.moves and pos not in p2.moves:
             p1.moves.append(pos)
-
 
         redrawWindow(win, p1, p2)
 

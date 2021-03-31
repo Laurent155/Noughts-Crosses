e6119de9495ee7c3e3048c6fa394166b08b764ce
Laurent155 <80858270+Laurent155@users.noreply.github.com>
2021-03-31 23:19:40 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self, turn):
+        self.turn = turn
+        self.p1moves = []
+        self.p2moves = []
+   
+    def draw(self, coord, win):
+        coord = coord[0] * 201 + 100, coord[1] * 201 + 100
+        if self.symbol == "circle":
+            pygame.draw.circle(win, (0, 0, 0), coord, 20, 5)
+        else:
+            pygame.draw.line(win, (0, 0, 0), (coord[0] - 20, coord[1] - 20), (coord[0] + 20, coord[1] + 20), 5)
+            pygame.draw.line(win, (0, 0, 0), (coord[0] + 20, coord[1] - 20), (coord[0] - 20, coord[1] + 20), 5)  
